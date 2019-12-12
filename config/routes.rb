0b8a02f30302c706f454016cc2ff51ad7c552f20
Ella <michaella.latkovic@gmail.com>
2019-12-12 14:16:44 -0800

    added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :todos
       resources :courses
-      resources :platforms
+      resources :platforms do
+        resources :courses do 
+          resources :todos
+        end
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
